282e4be0a2acb8b000817adb6935240e131354a6
kamal <k0k03lc@homeoffice.wal-mart.com>
2021-02-19 02:28:25 +0530

    flux element unit test

--- src/test/java/com/kishore/kamal/webflux/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/kishore/kamal/webflux/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.kishore.kamal.webflux.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -14,5 +15,32 @@ public class FluxAndMonoTest {
         flux.subscribe(System.out::println,
                 (e) -> System.err.println("Exception is "+e),
                 () -> System.out.println("Completed"));
+    }
+
+    @Test
+    public void fluxTestElement_withoutError(){
+        Flux<String> flux = Flux.just("Spring", "Spring boot", "Reactive Spring")
+//                .concatWith(Flux.error(new RuntimeException("Exception occured")))
+                .log();
+
+        StepVerifier.create(flux).expectNext("Spring").expectNext("Spring boot").expectNext("Reactive Spring").verifyComplete();
+    }
+
+    @Test
+    public void fluxTestElement_withError(){
+        Flux<String> flux = Flux.just("Spring", "Spring boot", "Reactive Spring")
+                .concatWith(Flux.error(new RuntimeException("Exception occured")))
+                .log();
+
+        StepVerifier.create(flux).expectNext("Spring").expectNext("Spring boot").expectNext("Reactive Spring").expectError(RuntimeException.class);
+    }
+
+    @Test
+    public void fluxTestElement_withError1(){
+        Flux<String> flux = Flux.just("Spring", "Spring boot", "Reactive Spring")
+                .concatWith(Flux.error(new RuntimeException("Exception occured")))
+                .log();
+
+        StepVerifier.create(flux).expectNext("Spring", "Spring boot", "Reactive Spring").expectError(RuntimeException.class);
     }
 }
